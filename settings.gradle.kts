9d7ee62db37ac7b5cbe6280ef00d6492f1fbd5c8
Rúben Ferreira && Rodrigo Meireles <ruben.a.m.ferreira@gmail.com>
2026-01-08 08:24:43 +0000

    projeto vazio

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Projeto_Cmen_Gestor_Receitas_Lista_compras"
+include(":app")
+ 
